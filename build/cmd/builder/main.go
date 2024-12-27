2c41be524c3c09eba6ea7acd40d16613dc3d65b1
Wojciech Malota-Wojcik <wojtek@exw.co>
2024-12-27 08:33:07 +0100

    fix

--- build/cmd/builder/main.go
+++ build/cmd/builder/main.go
@@ -4,12 +4,14 @@ import (
 	"github.com/outofforest/build/v2"
 	"github.com/outofforest/build/v2/pkg/tools/git"
 	"github.com/outofforest/tools"
+	"github.com/outofforest/tools/pkg/tools/golang"
 )
 
 func main() {
 	build.RegisterCommands(
 		build.Commands,
 		git.Commands,
+		golang.Commands,
 	)
 	tools.Main()
 }
